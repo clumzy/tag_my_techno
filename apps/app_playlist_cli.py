9f847a98ba3a0d20db16501f294eaa8d7d9f7b32
clumzy <george.pied@gmail.com>
2022-05-12 01:16:14 +0200

    Threw together a quick and dirty cli app using click. TBI

--- apps/app_playlist_cli.py
+++ apps/app_playlist_cli.py
@@ -1,15 +1,12 @@
+from email.policy import default
 from importlib.resources import files
-from unittest.util import sorted_list_difference
 import tagmytechno as tmt
-import pandas as pd
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 import os
 from p_tqdm import p_map
 from pathlib import Path
 from tqdm import tqdm
-from pprint import pprint
+import click
 
 def get_files_list(location = "/home/george/Code/clumzy/tag_my_techno/datasets/audio/"):
     audio_files = np.empty((0,1))
@@ -20,7 +17,9 @@ def get_files_list(location = "/home/george/Code/clumzy/tag_my_techno/datasets/a
 
 def predict_and_sort_audio(model, files_list, threshold = 0.85):
     sorted_list = {}
+    print(f"Creating images.")
     images = list(p_map(tmt.song_to_img, files_list, num_cpus=6))
+    print(f"Generating genre predictions.")
     results = [tmt.get_genre_prediction(model, img, threshold) for img in tqdm(images)]
     for tune, result in zip(files_list,results):
         for g in result:
@@ -36,8 +35,23 @@ def playlist_from_list(m3u_location, audio_list):
         f.write(str(audio)+"\n")
     f.close()
 
-model = tmt.create_model("/home/george/Code/clumzy/tag_my_techno/apps/mdl.keras")
-files = get_files_list("/media/george/George/music/2203")
-lst = predict_and_sort_audio(model, files, 0.95)
-for playlist in lst:
-    playlist_from_list(os.path.join('/home/george/Code/clumzy/tag_my_techno/apps/example_playlists',f"{playlist.lower().replace(' ', '_')}.m3u"), lst[playlist])
+
+
+@click.command()
+@click.option('--model_loc', default="mdl.keras", help='The location of the model.', show_default=True)
+@click.option('--audio_loc', help='The location of the audio files to guess.', show_default=True)
+@click.option('--playlist_loc', help='The location where you want the playlists to be stored.', show_default=True)
+@click.option('--threshold', default=0.95, help="The threshold above which you wish the genre to be recognized.", show_default=True)
+def sorter(model_loc, audio_loc, playlist_loc, threshold):
+    model = tmt.create_model(model_loc)
+    files = get_files_list(audio_loc)
+    print(f"Now analyzing {len(files)} files :")
+    lst = predict_and_sort_audio(model, files, threshold)
+    print(f"Now creating {len(lst)} playlists.")
+    for playlist in lst:
+        playlist_from_list(os.path.join(playlist_loc,f"{playlist.lower().replace(' ', '_')}.m3u"), lst[playlist])
+    pass
+
+
+if __name__ == '__main__':
+    sorter()
